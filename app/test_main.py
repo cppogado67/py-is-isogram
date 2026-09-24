0d0fbaa97f5d4a0ee1cd3a6b53068277ebd7b4c3
claro <c_ppogado67@yahoo.com>
2026-09-24 17:32:37 +0800

    003

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import is_isogram
 
 
-def test_playgrounds_is_isogram():
-	assert is_isogram("playgrounds") is True
+def test_playgrounds_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
 
 
-def test_look_is_not_isogram():
-	assert is_isogram("look") is False
+def test_look_is_not_isogram() -> None:
+    assert is_isogram("look") is False
 
 
-def test_adam_is_not_isogram_case_insensitive():
-	assert is_isogram("Adam") is False
+def test_adam_is_not_isogram_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
 
 
-def test_empty_string_is_isogram():
-	assert is_isogram("") is True
+def test_empty_string_is_isogram() -> None:
+    assert is_isogram("") is True
